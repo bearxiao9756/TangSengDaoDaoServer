18c326dd1a2089e3a7e542d5dd1cbbcd807216c4
bearxiao9756 <bearxiao9@gmail.com>
2025-11-01 00:09:20 +0800

    新域名

--- modules/group/api.go
+++ modules/group/api.go
@@ -275,20 +275,20 @@ func (g *Group) avatarGet(c *wkhttp.Context) {
 		c.Writer.WriteHeader(http.StatusInternalServerError)
 		return
 	}
-	// subkehu := "https://hy82s2hjk23.icu:9000/";
-	// subkehu1 := "http://hy82s2hjk23.icu:9000/";
-	// subkehu2 := "http://hy82s2hjk23.icu:8090/";
-	// subkehuT := "https://hy82s2hjk23.icu/img/";
+	subkehu := "https://123gov.icu:9000/";
+	subkehu1 := "http://123gov.icu:9000/";
+	subkehu2 := "http://123gov.icu:8090/";
+	subkehuT := "https://123gov.icu/img/";
 
-    // if(strings.Contains(downloadUrl,subkehu)){
-	//   downloadUrl = strings.ReplaceAll(downloadUrl,subkehu,subkehuT)
-	// }
-	// if(strings.Contains(downloadUrl,subkehu1)){
-	//   downloadUrl = strings.ReplaceAll(downloadUrl,subkehu1,subkehuT)
-	// }
-	// if(strings.Contains(downloadUrl,subkehu2)){
-	//   downloadUrl = strings.ReplaceAll(downloadUrl,subkehu2,subkehuT)
-	// }
+    if(strings.Contains(downloadUrl,subkehu)){
+	  downloadUrl = strings.ReplaceAll(downloadUrl,subkehu,subkehuT)
+	}
+	if(strings.Contains(downloadUrl,subkehu1)){
+	  downloadUrl = strings.ReplaceAll(downloadUrl,subkehu1,subkehuT)
+	}
+	if(strings.Contains(downloadUrl,subkehu2)){
+	  downloadUrl = strings.ReplaceAll(downloadUrl,subkehu2,subkehuT)
+	}
 	if strings.Contains(downloadUrl, "?") {
 		c.Redirect(http.StatusFound, fmt.Sprintf("%s&v=%s", downloadUrl, v))
 	} else {
--- modules/user/api.go
+++ modules/user/api.go
@@ -416,20 +416,20 @@ func (u *User) UserAvatar(c *wkhttp.Context) {
 			return
 		}
 	}
-    // http://hy82s2hjk23.icu:9000/avatar/45/40647119634e45359290fd5a3038f7df.png?response-content-disposition=inline%3B+filename%3D%2240647119634e45359290fd5a3038f7df.png%22&v=1759734479417
-    // subkehu := "https://hy82s2hjk23.icu:9000/";
-	// subkehu1 := "http://hy82s2hjk23.icu:9000/";
-	// subkehu2 := "http://hy82s2hjk23.icu:8090/";
-	// subkehuT := "https://hy82s2hjk23.icu/img/";
-    // if(strings.Contains(downloadUrl,subkehu)){
-	//   downloadUrl = strings.ReplaceAll(downloadUrl,subkehu,subkehuT)
-	// }
-	// if(strings.Contains(downloadUrl,subkehu1)){
-	//   downloadUrl = strings.ReplaceAll(downloadUrl,subkehu1,subkehuT)
-	// }
-	// if(strings.Contains(downloadUrl,subkehu2)){
-	//   downloadUrl = strings.ReplaceAll(downloadUrl,subkehu2,subkehuT)
-	// }
+    // http://123gov.icu:9000/avatar/45/40647119634e45359290fd5a3038f7df.png?response-content-disposition=inline%3B+filename%3D%2240647119634e45359290fd5a3038f7df.png%22&v=1759734479417
+    subkehu := "https://123gov.icu:9000/";
+	subkehu1 := "http://123gov.icu:9000/";
+	subkehu2 := "http://123gov.icu:8090/";
+	subkehuT := "https://123gov.icu/img/";
+    if(strings.Contains(downloadUrl,subkehu)){
+	  downloadUrl = strings.ReplaceAll(downloadUrl,subkehu,subkehuT)
+	}
+	if(strings.Contains(downloadUrl,subkehu1)){
+	  downloadUrl = strings.ReplaceAll(downloadUrl,subkehu1,subkehuT)
+	}
+	if(strings.Contains(downloadUrl,subkehu2)){
+	  downloadUrl = strings.ReplaceAll(downloadUrl,subkehu2,subkehuT)
+	}
 	if strings.Contains(downloadUrl, "?") {
 		c.Redirect(http.StatusFound, fmt.Sprintf("%s&v=%s", downloadUrl, v))
 	} else {
