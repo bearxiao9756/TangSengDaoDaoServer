e3db5eaa6d9863e1e55274bb171ed9b1a0f0fb75
bearxiao9756 <bearxiao9@gmail.com>
2025-11-10 09:55:15 +0800

    服务名称更换

--- modules/robot/const.go
+++ modules/robot/const.go
@@ -20,8 +20,8 @@ const (
 var systemRobotMap = []*systemRobotMenu{
 	{
 		CMD:          "/基本信息",
-		Remark:       "坤元服务基本信息",
-		ReplyContent: "坤元服务是一款轻量级，高性能，重安全专注于私有化部署的开源即时通讯系统。坤元服务官网：https://www.tsdaodao.com 各端演示地址：https://tsdaodao.com/guide/demo.html 悟空官网：https://githubim.com 在APP我的-设置-模块管理中关闭所有模块即是开源版本所有功能。",
+		Remark:       "广安服务基本信息",
+		ReplyContent: "广安服务是一款轻量级，高性能，重安全专注于私有化部署的开源即时通讯系统。广安服务官网：https://www.tsdaodao.com 各端演示地址：https://tsdaodao.com/guide/demo.html 悟空官网：https://githubim.com 在APP我的-设置-模块管理中关闭所有模块即是开源版本所有功能。",
 		Type:         string(None),
 	},
 	{
@@ -50,8 +50,8 @@ var systemRobotMap = []*systemRobotMenu{
 	},
 	{
 		CMD:          "/Android包下载",
-		Remark:       "如何下载坤元服务 Android包",
-		ReplyContent: "您好，坤元服务 Android应用下载地址 https://www.pgyer.com/tsdd",
+		Remark:       "如何下载广安服务 Android包",
+		ReplyContent: "您好，广安服务 Android应用下载地址 https://www.pgyer.com/tsdd",
 		Type:         string(None),
 	},
 	// {
