36e53fedf166a0f16b10c7e6491914bda3db6989
bearxiao9756 <bearxiao9@gmail.com>
2025-11-11 19:45:00 +0800

    更新域名

--- modules/user/api.go
+++ modules/user/api.go
@@ -476,80 +476,10 @@ func (u *User) UserAvatar(c *wkhttp.Context) {
 			return
 		}
 	}
-	// http://abhao.icu:9000/avatar/45/40647119634e45359290fd5a3038f7df.png?response-content-disposition=inline%3B+filename%3D%2240647119634e45359290fd5a3038f7df.png%22&v=1759734479417
-	// subkehu := "https://abhao.icu:9000/"
-	// subkehu1 := "http://abhao.icu:9000/"
-	// subkehu2 := "http://abhao.icu:8090/"
-	// subkehuT := "https://abhao.icu/img/"
-	// if strings.Contains(downloadUrl, subkehu) {
-	// 	downloadUrl = strings.ReplaceAll(downloadUrl, subkehu, subkehuT)
-	// }
-	// if strings.Contains(downloadUrl, subkehu1) {
-	// 	downloadUrl = strings.ReplaceAll(downloadUrl, subkehu1, subkehuT)
-	// }
-	// if strings.Contains(downloadUrl, subkehu2) {
-	// 	downloadUrl = strings.ReplaceAll(downloadUrl, subkehu2, subkehuT)
-	// }
-
-	// subkehua2 := "https://123edu.icu:9000/"
-	// subkehu12 := "http://123edu.icu:9000/"
-	// subkehu22 := "http://123edu.icu:8090/"
-	// subkehuT2 := "https://123edu.icu/img/"
-
-	// if strings.Contains(downloadUrl, subkehua2) {
-	// 	downloadUrl = strings.ReplaceAll(downloadUrl, subkehua2, subkehuT2)
-	// }
-	// if strings.Contains(downloadUrl, subkehu12) {
-	// 	downloadUrl = strings.ReplaceAll(downloadUrl, subkehu12, subkehuT2)
-	// }
-	// if strings.Contains(downloadUrl, subkehu22) {
-	// 	downloadUrl = strings.ReplaceAll(downloadUrl, subkehu22, subkehuT2)
-	// }
-
-	// subkehua20 := "https://bao.abhao.icu:9000/"
-	// subkehu120 := "http://bao.abhao.icu:9000/"
-	// subkehu220 := "http://bao.abhao.icu:8090/"
-	// subkehuT20 := "https://bao.abhao.icu/img/"
-
-	// if strings.Contains(downloadUrl, subkehua20) {
-	// 	downloadUrl = strings.ReplaceAll(downloadUrl, subkehua20, subkehuT20)
-	// }
-	// if strings.Contains(downloadUrl, subkehu120) {
-	// 	downloadUrl = strings.ReplaceAll(downloadUrl, subkehu120, subkehuT20)
-	// }
-	// if strings.Contains(downloadUrl, subkehu220) {
-	// 	downloadUrl = strings.ReplaceAll(downloadUrl, subkehu220, subkehuT20)
-	// }
-	subkehua21 := "https://bao.aehao.icu:9000/"
-	subkehu121 := "http://bao.aehao.icu:9000/"
-	subkehu221 := "http://bao.aehao.icu:8090/"
-	subkehuT21 := "https://bao.aehao.icu/img/"
-	if strings.Contains(downloadUrl, subkehua21) {
-		downloadUrl = strings.ReplaceAll(downloadUrl, subkehua21, subkehuT21)
-	}
-	if strings.Contains(downloadUrl, subkehu121) {
-		downloadUrl = strings.ReplaceAll(downloadUrl, subkehu121, subkehuT21)
-	}
-	if strings.Contains(downloadUrl, subkehu221) {
-		downloadUrl = strings.ReplaceAll(downloadUrl, subkehu221, subkehuT21)
-	}
-	// subkehua22 := "https://wen.aehao.icu:9000/"
-	// subkehu122 := "http://wen.aehao.icu:9000/"
-	// subkehu222 := "http://wen.aehao.icu:8090/"
-	// subkehuT22 := "https://wen.aehao.icu/img/"
-	// if strings.Contains(downloadUrl, subkehua22) {
-	// 	downloadUrl = strings.ReplaceAll(downloadUrl, subkehua22, subkehuT22)
-	// }
-	// if strings.Contains(downloadUrl, subkehu121) {
-	// 	downloadUrl = strings.ReplaceAll(downloadUrl, subkehu122, subkehuT22)
-	// }
-	// if strings.Contains(downloadUrl, subkehu222) {
-	// 	downloadUrl = strings.ReplaceAll(downloadUrl, subkehu222, subkehuT22)
-	// }
-	subkehua23 := "https://hao.aehao.icu:9000/"
-	subkehu123 := "http://hao.aehao.icu:9000/"
-	subkehu223 := "http://hao.aehao.icu:8090/"
-	subkehuT23 := "https://hao.aehao.icu/img/"
+	subkehua23 := "https://a.imhao.icu:9000/"
+	subkehu123 := "http://a.imhao.icu:9000/"
+	subkehu223 := "http://a.imhao.icu:8090/"
+	subkehuT23 := "https://a.imhao.icu/img/"
 	if strings.Contains(downloadUrl, subkehua23) {
 		downloadUrl = strings.ReplaceAll(downloadUrl, subkehua23, subkehuT23)
 	}
