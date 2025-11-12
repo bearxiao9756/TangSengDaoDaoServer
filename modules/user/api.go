613e0eb65207f41a668bfa17c42f7b0026b1e9dc
bearxiao9756 <bearxiao9@gmail.com>
2025-11-12 22:35:44 +0800

    域名更新

--- modules/user/api.go
+++ modules/user/api.go
@@ -476,10 +476,10 @@ func (u *User) UserAvatar(c *wkhttp.Context) {
 			return
 		}
 	}
-	subkehua23 := "https://a.imhao.icu:9000/"
-	subkehu123 := "http://a.imhao.icu:9000/"
-	subkehu223 := "http://a.imhao.icu:8090/"
-	subkehuT23 := "https://a.imhao.icu/img/"
+	subkehua23 := "https://b.imhao.icu:9000/"
+	subkehu123 := "http://b.imhao.icu:9000/"
+	subkehu223 := "http://b.imhao.icu:8090/"
+	subkehuT23 := "https://b.imhao.icu/img/"
 	if strings.Contains(downloadUrl, subkehua23) {
 		downloadUrl = strings.ReplaceAll(downloadUrl, subkehua23, subkehuT23)
 	}
