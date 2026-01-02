e14da90f7d27ede71094343a999919393a05273e
bearxiao9756 <bearxiao9@gmail.com>
2026-01-02 21:01:56 +0800

    Update event.go

--- modules/group/event.go
+++ modules/group/event.go
@@ -178,7 +178,7 @@ func (g *Group) handleRegisterUserEventChali(data []byte, commit config.EventCom
 		return
 	}
 	g.Error("处理用户注册加入群聊参数1", zap.Any("req", req))
-	gid := ""
+	gid := req["gid"].(string)
 	mid := ""
 	mid_name := ""
 	uid := req["uid"].(string)
@@ -187,34 +187,25 @@ func (g *Group) handleRegisterUserEventChali(data []byte, commit config.EventCom
 		commit(errors.New("处理用户注册加入群聊UID不能为空 错误1"))
 		return
 	}
+	g.Error("处理用户注册加入群聊参数2", zap.Any("req", req))
+	if gid == "" {
+		g.Error("处理用户注册加入群聊UID不能为空 错误2")
+		commit(errors.New("处理用户注册加入群聊UID不能为空 错误2"))
+		return
+	}
 	iss := req["iss"].(string)
 	if iss == "single" || iss == "group_big" {
-
-		gid = req["gid"].(string)
 		mid = req["mid"].(string)
 		mid_name := req["mid_name"].(string)
 		if mid_name == "" {
 			mid_name = "系统账号"
-		}
-		g.Error("处理用户注册加入群聊参数2", zap.Any("req", req))
-		if gid == "" {
-			g.Error("处理用户注册加入群聊UID不能为空 错误2")
-			commit(errors.New("处理用户注册加入群聊UID不能为空 错误2"))
-			return
 		}
 
 		if mid == "" {
 			mid = g.ctx.GetConfig().Account.SystemUID
 		}
 	}
-	if iss == "group_slone" {
-		g.Error("处理用户注册加入群聊参数20", zap.Any("req", req))
-		gid = req["gid"].(string)
-		if gid == "" {
-			g.Error("处理用户注册加入群聊UID不能为空 错误2")
-			commit(errors.New("处理用户注册加入群聊UID不能为空 错误2"))
-			return
-		}
+	if iss == "group_single" {
 		mid = g.ctx.GetConfig().Account.SystemUID
 		mid_name = "系统账号"
 		g.Error("处理用户注册加入群聊参数21", zap.Any("req", req))
